fd0b9ad4891a8c4346d16987f95064ca4ffae2ee
Tiago Ling Alexandre <ling.tiago@gmail.com>
2017-12-19 10:14:45 -0200

    Basic setup - Class stubs + starter content used in project (particles, materials and meshes)

---
+++ Source/BomberClone/BomberCloneGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BomberCloneGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOMBERCLONE_API ABomberCloneGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
